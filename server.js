3fdcd2478581d4926e9c45cac3615bb86235783d
Shardul-2323 <shardulmore2323@gmail.com>
2020-11-02 18:43:02 +0530

    deploy hotelapp

---
+++ server.js
@@ -0,0 +1,15 @@
+const express=require('express');
+const http=require('http');
+const path=require('path');
+const app=express();
+const port=9999;
+
+app.use(express.static(path.join(__dirname,'build')));
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'build/index.html'))
+})
+
+app.listen(port,(err)=>{
+    if (err) throw err;
+    console.log('Server is running on port')
+})
